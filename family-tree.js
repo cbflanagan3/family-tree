c9bde8e312177aa6654b1a020debeaed7058aeaf
Caroline B Flanagan <carolineflan@gmail.com>
2020-04-27 18:35:56 -0400

    familytree

--- family-tree.js
+++ family-tree.js
@@ -1,5 +1,41 @@
 class FamilyTree {
-  constructor () {}
+  constructor (value) {
+    this.value = value;
+    this.children = [];
+    if (!(this.value)) {
+      throw Error;
+    } 
+    
+  if (typeof this.value !== 'string') {
+      throw Error;
+    }
+  }
+  insert (value) {
+    const child = new FamilyTree(value);
+    if (child) {
+      this.children.push(child);
+    }
+  }
+
+  familySize () {
+      let familySize = this.children.length + 1;
+      return familySize;
+  }
+
+  findMember (value) {
+    debugger;
+    if (this.value === value) {
+      return this.value;
+    } else if (value.children !== null) {
+      for (let i = 0; i == null && i < value.children.length; i++) {
+        return findMember(value.children[i])
+      }
+    }
+  }
+
+  log (value) {
+    return '--`${this.value}`';
+  }
 }
 
 module.exports = FamilyTree;
